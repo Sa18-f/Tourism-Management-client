790a93e7d4cc81d4a19afeb37eac553d8a775936
Sa18-f <sisazid182713@gmail.com>
2024-04-26 02:03:52 +0600

    add spot form created

--- src/Components/AddSpots/AddSpots.jsx
+++ src/Components/AddSpots/AddSpots.jsx
@@ -1,9 +1,151 @@
-
+import Swal from 'sweetalert2'
 
 const AddSpots = () => {
+    const handleAddSpot = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const quantity = form.quantity.value;
+        const supplier = form.supplier.value;
+        const taste = form.taste.value;
+        const category = form.category.value;
+        const details = form.details.value;
+        const photo = form.photo.value;
+        const newCoffee = { name, quantity, supplier, taste, category, details, photo };
+        console.log(newCoffee);
+        fetch('http://localhost:5000/coffee', {
+            method: "POST",
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newCoffee)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.insertedId) {
+                    Swal.fire({
+                        title: 'Success!',
+                        text: 'Coffee added successfully',
+                        icon: 'success',
+                        confirmButtonText: 'Close'
+                    })
+                }
+            })
+    }
     return (
-        <div>
-            <h1>Where you need to add </h1>
+        <div className="bg-[#F4F3F0] p-24">
+            <h2 className="text-3xl font-extrabold">Add a Spot</h2>
+            <form onSubmit={handleAddSpot}>
+                {/* form name and quantity row */}
+                <div className="md:flex mb-8">
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text">Spot Name</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="tourists_spot_name" placeholder="Spot Name" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                    <div className="form-control md:w-1/2 ml-4">
+                        <label className="label">
+                            <span className="label-text">Location</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="location" placeholder="Location" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                </div>
+                {/* form supplier row */}
+                <div className="md:flex mb-8">
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text">Country Name</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="country_Name" placeholder="Country Name" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                    <div className="form-control md:w-1/2 ml-4">
+                        <label className="label">
+                            <span className="label-text">Average Cost</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="average_cost" placeholder="Average Cost" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                </div>
+                {/* form category and details row */}
+                <div className="md:flex mb-8">
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text">Seasonality</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="seasonality" placeholder="Seasonality" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                    <div className="form-control md:w-1/2 ml-4">
+                        <label className="label">
+                            <span className="label-text">Travel Time</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="travel_time" placeholder="Travel Time" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                </div>
+                {/* form visitors count */}
+                <div className="md:flex mb-8">
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text">Total Visitors Per Year</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="totalVisitorsPerYear" placeholder="Total Visitors Per Year" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                    <div className="form-control md:w-1/2 ml-4">
+                        <label className="label">
+                            <span className="label-text">Description</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="description" placeholder="Description" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                </div>
+                {/* form email and name */}
+                <div className="md:flex mb-8">
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text">Your Name</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="name" placeholder="Your Name" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                    <div className="form-control md:w-1/2 ml-4">
+                        <label className="label">
+                            <span className="label-text">Your Email</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="email" placeholder="Your email" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                </div>
+                {/* form Photo url row */}
+                <div className="mb-8">
+                    <div className="form-control w-full">
+                        <label className="label">
+                            <span className="label-text">Photo URL</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name="photo" placeholder="Photo URL" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                </div>
+                <input type="submit" value="Add" className="btn btn-block bg-green-400 text-xl" />
+
+            </form>
         </div>
     );
 };
